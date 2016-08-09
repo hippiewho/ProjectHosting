819b815640a9db7cd7288af5a39485be710e9a17
Frank <Frank@DESKTOP-7T74Q1U>
2016-08-08 20:19:49 -0700

    Added folder DATETIME search

--- myWebsite/Controllers/HomeController.cs
+++ myWebsite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,8 +13,30 @@ namespace myWebsite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-          //  Server.MapPath();
-            //ViewBag.ListOfIMG = new List<String>(Directory.EnumerateDirectories)
+            
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(Server.MapPath("~")));
+            String[,] DTDirs = new String[3, 2];
+            
+            foreach (string dir in dirs)
+            {
+                if (DTDirs[0, 0] == null || Convert.ToDateTime(DTDirs[0, 1]) < Directory.GetLastWriteTime(dir))
+                {
+                    DTDirs[0, 0] = dir;
+                    DTDirs[0, 1] = Directory.GetLastWriteTime(dir).ToString();
+                }
+                else if (DTDirs[1, 0] == null || Convert.ToDateTime(DTDirs[1, 1]) < Directory.GetLastWriteTime(dir))
+                {
+                    DTDirs[1, 0] = dir;
+                    DTDirs[1, 1] = Directory.GetLastWriteTime(dir).ToString();
+                }
+                else if (DTDirs[2, 0] == null || Convert.ToDateTime(DTDirs[2, 1]) < Directory.GetLastWriteTime(dir))
+                {
+                    DTDirs[2, 0] = dir;
+                    DTDirs[2, 1] = Directory.GetLastWriteTime(dir).ToString();
+                }
+            }
+
+            ViewBag.dirs = DTDirs;
             return View();
         }
     }
