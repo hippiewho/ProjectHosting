d0d134ecdcb0db07710047cb22c15afa75deff2a
Frank Navarrete <hippiewho@yahoo.com>
2018-03-22 22:23:13 -0700

    Added Api Routing

---
+++ myWebsite/Controllers/ProjectApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using myWebsite.Models;
+
+namespace myWebsite.Controllers
+{
+    public class ProjectApiController : ApiController
+    {
+        private ProjectContext projectContext { get; } = new ProjectContext();
+
+        // GET api/ProjectApiController
+        public IEnumerable<ProjectModel> Get()
+        {
+            
+            return projectContext.ProjectList.AsEnumerable();
+        }
+
+        // GET api/ProjectApiController/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- myWebsite/Global.asax.cs
+++ myWebsite/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Data.Entity;
+using System.Web.Http;
+using myWebsite.App_Start;
+
 namespace myWebsite
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -14,6 +17,7 @@ namespace myWebsite
             Database.SetInitializer<myWebsite.Models.LoginContext>(null);
             System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = System.Security.Claims.ClaimTypes.Name;
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
