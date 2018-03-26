ab30c28f214630e681eef618ad647b1f6ca8b06b
Frank Navarrete <hippiewho@yahoo.com>
2018-03-25 21:23:13 -0700

    Changes to project model. Trims String Fields

--- myWebsite/Global.asax.cs
+++ myWebsite/Global.asax.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using myWebsite.App_Start;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Data.Entity;
-using System.Web.Http;
-using myWebsite.App_Start;
 
 namespace myWebsite
 {
--- myWebsite/Models/ProjectViewModel.cs
+++ myWebsite/Models/ProjectViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace myWebsite.Models
@@ -19,39 +16,53 @@ namespace myWebsite.Models
     [Table("Projects")]
     public class ProjectModel
     {
-        [Required]
-        [Display(Name = "Id")]
-        [DataType(DataType.Text)]
+        [Required] [Display(Name = "Id")] [DataType(DataType.Text)]
         public int Id { get; set; }
 
-        [Required]
-        [Display(Name = "Name")]
-        [DataType(DataType.Text)]
-        public string Name { get; set; }
+        [Required] [Display(Name = "Name")] [DataType(DataType.Text)]
+        private string _name;
 
-       
-        [Display(Name = "Description")]
-        [DataType(DataType.Text)]
-        public string Description { get; set; }
 
-        
-        [Display(Name = "Url")]
-        [DataType(DataType.Url)]
-        public string Url { get; set; }
+        [Display(Name = "Description")] [DataType(DataType.Text)]
+        private string _description;
 
-        [DataType(DataType.ImageUrl)]
-        [Display(Name = "ImagePath")]
-        public string ImagePath { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Position")]
+        [Display(Name = "Url")] [DataType(DataType.Url)]
+        private string _url;
+
+        [DataType(DataType.ImageUrl)] [Display(Name = "ImagePath")]
+        private string _imagePath;
+
+        [Required] [DataType(DataType.Text)] [Display(Name = "Position")]
         public byte Position { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "UserId")]
+        [Required] [DataType(DataType.Text)] [Display(Name = "UserId")]
         public int UserId { get; set; }
+
+        public string Name
+        {
+            get => _name.Trim();
+
+            set => _name = value;
+        }
+        public string Description
+        {
+            get => _description.Trim();
+
+            set => _description = value;
+        }
+        public string Url
+        {
+            get => _url.Trim();
+
+            set => _url = value;
+        }
+        public string ImagePath
+        {
+            get => _imagePath.Trim();
+
+            set => _imagePath = value;
+        }
 
     }
 }
